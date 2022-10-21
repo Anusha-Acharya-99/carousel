01e7371c5d063860a440651ee57d44ad89d5527f
Anusha Acharya <acharanusha99@gmail.com>
2022-10-21 18:32:00 +0530

    displaying image and adding prev & next button

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./index.css";
+import prev from "../../assets/icons/prev-icon.png";
+import next from "../../assets/icons/next-icon.png";
+import { carouselData } from "../../data";
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  return (
+    <div className="carousel-container">
+      <img className="buttons" src={prev} alt="" />
+      <img className="carousel-image" src={carouselData[0].url} alt="" />
+      <img className="buttons" src={next} alt="" />
+    </div>
+  );
 };
 
 export default Carousel;
